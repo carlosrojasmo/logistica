f4c20677e9a9ea011ea75fa28f24e5e946b3ea27
Carlos <carlos.rojasmo@sansano.usm.com>
2020-10-17 23:20:16 -0300

    casi

--- server/logistica.go
+++ server/logistica.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"google.golang.org/grpc"
 	pb "../proto"
+	"fmt"
 )
 const (
 	port = ":50051"
@@ -15,8 +16,8 @@ const (
 var colaRetail=[]paquete{}
 var colaPrioritario=[]paquete{}
 var colaNormal=[]paquete{}
-var registro map[int]orden
-
+var registro= make(map[int]orden)
+var registroPaquete= make (map[int]paquete)
 type server struct {
 	pb.UnimplementedOrdenServiceServer
 }
@@ -31,12 +32,12 @@ type orden struct {
 	destino string
 	seguimiento int
 }
-func newOrden(tipo string,nombre string,valor int,origen string, destino string,id string) *orden{
+func newOrden(tipo string,nombre string,valor int,origen string, destino string,id string) orden{
 	ordenNueva := orden{nombre: nombre,tipo: tipo,valor: valor ,origen: origen,idPaquete: id, destino: destino}
 	random := rand.NewSource(time.Now().UnixNano())
 	ordenNueva.seguimiento=(rand.New(random)).Intn(492829)
 	ordenNueva.timestamp=time.Now()
-	return &ordenNueva
+	return ordenNueva
 }
 type paquete struct {
 	idPaquete string
@@ -47,44 +48,50 @@ type paquete struct {
 	estado string
 }
 
-func newPaquete(idPaquete string, tipo string, valor int) *paquete{
+func newPaquete(idPaquete string, tipo string, valor int) paquete{
 	paqueteNuevo := paquete{idPaquete: idPaquete, tipo: tipo,valor: valor}
 	random := rand.NewSource(time.Now().UnixNano())
 	paqueteNuevo.seguimiento=(rand.New(random)).Intn(492829)
 	paqueteNuevo.intentos= 0;
-	paqueteNuevo.estado="en bodega"
-	return &paqueteNuevo
+	paqueteNuevo.estado="Recibido"
+	return paqueteNuevo
 }
-func buscarPaquete(seguimiento int) orden{
-	return registro[seguimiento]
+func buscarPaquete(seguimiento int) paquete{
+	return registroPaquete[seguimiento]
 }
 
 func recibir(mensaje orden) orden{
 	nuevaOrden :=newOrden(mensaje.tipo,mensaje.nombre,mensaje.valor,mensaje.origen,mensaje.destino,mensaje.idPaquete)
-	//Aqui enviar nuevaOrden.seguimiento a cliente()
+	fmt.Println(nuevaOrden)
 	nuevoPaquete := newPaquete(mensaje.idPaquete,mensaje.tipo,mensaje.valor)
+	registroPaquete[nuevoPaquete.seguimiento]=nuevoPaquete
 	if nuevoPaquete.tipo=="retail"{
-		colaRetail=append(colaRetail,*nuevoPaquete)
+		colaRetail=append(colaRetail,nuevoPaquete)
 	} else if nuevoPaquete.tipo=="normal"{
-		colaNormal=append(colaNormal,*nuevoPaquete)
+		colaNormal=append(colaNormal,nuevoPaquete)
 	} else{
-		colaPrioritario=append(colaPrioritario, *nuevoPaquete)
+		colaPrioritario=append(colaPrioritario, nuevoPaquete)
 	}
-	registro[nuevaOrden.seguimiento]= *nuevaOrden
-	return *nuevaOrden
+	registro[nuevaOrden.seguimiento]= nuevaOrden
+	return nuevaOrden
 }
 
 func enviarColas(){
 	
 }
 
-func (s* server)replyToOrder(ctx context.Context,pedido *pb.SendToOrden) (*pb.ReplyFromOrden,error){
-	orden := *newOrden(pedido.Tipo,pedido.Nombre,int(pedido.Valor),pedido.Origen,pedido.Destino,pedido.IdPaquete)
+func (s* server)ReplyToOrder(ctx context.Context,pedido *pb.SendToOrden) (*pb.ReplyFromOrden,error){
+	orden := newOrden(pedido.Tipo,pedido.Nombre,int(pedido.Valor),pedido.Origen,pedido.Destino,pedido.IdPaquete)
 	orden=recibir(orden)
 	seguimiento := pb.ReplyFromOrden{Seguimiento:int64(orden.seguimiento)}
 	return &seguimiento,nil
 }
+func (s* server)GetState(ctx context.Context, seguimiento *pb.ReplyFromOrden) (*pb.InfoSeguimiento, error){
+	paq:=buscarPaquete(int(seguimiento.Seguimiento))
+	estado:=pb.InfoSeguimiento{Estado:paq.estado}
 
+	return &estado,nil
+}
 
 func main() { 
 	lis, err := net.Listen("tcp", port)
